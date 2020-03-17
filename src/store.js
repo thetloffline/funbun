04ed1284c47756c9b7b96f4d7d83086b7cd8f1b9
thetloffline <thetloffline@users.noreply.github.com>
2020-03-17 17:37:02 +0200

    data store logic. work in progress

--- src/store.js
+++ src/store.js
@@ -1,11 +1,39 @@
 import Vue from 'vue'
-import vuex from 'vuex'
+import Vuex from 'vuex'
 import axios from 'axios'
 
-Vue.use(vuex, axios)
+Vue.use(Vuex, axios)
 
-export default new vuex.Store({
-    state: { 
-        test: 'testing 123'
-    }  
+const url = 'http://localhost:3000/api/files/'
+export default new Vuex.Store({
+  state: { 
+    cakes: []
+  },
+  mutations: {
+    SET_CAKES (state, cakesData) {
+      state.cakes = cakesData
+    }
+  },
+  actions: {
+    async loadCakes ({ commit }) {
+      const res = await axios.get(url)
+      .then(res => {
+        let cakesData = res.data
+        console.log(cakesData)
+        commit('SET_CAKES', cakesData)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    },
+    getCakes ({ commit }) {
+      this.getAllCakes()
+    }
+  },
+  getters: {
+    getAllCakes (state) { 
+      console.log(state.cakes)
+      return state.cakes
+  }
+  }
 })

0a2b20c4bca7a2e6e7cba1b304f502e1aaa97222
thetloffline <thetloffline@users.noreply.github.com>
2020-03-20 19:14:33 +0200

    now  handling POST requests to API

--- src/store.js
+++ src/store.js
@@ -9,31 +9,55 @@ export default new Vuex.Store({
   state: { 
     cakes: []
   },
+
   mutations: {
     SET_CAKES (state, cakesData) {
       state.cakes = cakesData
     }
   },
+
   actions: {
     async loadCakes ({ commit }) {
-      const res = await axios.get(url)
-      .then(res => {
-        let cakesData = res.data
-        console.log(cakesData)
-        commit('SET_CAKES', cakesData)
+      try {
+        return new Promise( async (resolve, reject) => {
+          const res = await axios.get(url)
+          let cakesData = res.data
+          resolve(
+            commit('SET_CAKES', cakesData)
+            )
+          })
+        } catch (err) {
+          console.log(err)
+        }
+      },
+      
+      async addNewCake ({ commit }, payload) {
+        return new Promise( async (resolve, reject) => {
+          try {
+            const response =  await axios.post(url, payload,{ headers: {
+              'Content-Type': 'multipart/form-data'
+            }})
+            resolve(
+              this.dispatch('loadCakes')
+              )
+              console.log(response.data)
+            } catch (error) {
+              reject(
+                'addNewCake error', error
+                )
+              }
+            })
+          },
+          
+          getCakes ({ commit }) {
+            this.getAllCakes()
+          }
+        },
+        
+        getters: {
+          getAllCakes (state) { 
+            console.log(state.cakes)
+            return state.cakes
+          }
+        }
       })
-      .catch(error => {
-        console.log(error)
-      })
-    },
-    getCakes ({ commit }) {
-      this.getAllCakes()
-    }
-  },
-  getters: {
-    getAllCakes (state) { 
-      console.log(state.cakes)
-      return state.cakes
-  }
-  }
-})

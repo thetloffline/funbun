597ab4ff1365c7e629161e87e6df5037e647e89c
thetloffline <thetloffline@users.noreply.github.com>
2020-03-23 13:39:09 +0200

    add comment

--- be/routes/api/files.js
+++ be/routes/api/files.js
@@ -19,6 +19,7 @@ router.post('/', async (req, res) => {
   const cake = {
     "cafeName" : req.body.cafeName,
     "location" : req.body.location,
+    "comment" : req.body.comment,
     "imageFile" : fileName, 
     "looks" : req.body.looks,
     "taste" : req.body.taste,

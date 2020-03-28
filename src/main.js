487f98b69d52f4e28b35414c29d67f0d90eb415f
thetloffline <thetloffline@users.noreply.github.com>
2020-03-28 14:09:31 +0200

    add global capitalize filter

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import fileUpload from './components/fileUpload.vue'
 import allCakes from './components/allCakes.vue'
 Vue.config.productionTip = false
 
+Vue.filter('capitalize', function (value) {
+  if (!value) return ''
+  value = value.toString()
+  return value.charAt(0).toUpperCase() + value.slice(1)
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',

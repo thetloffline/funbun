9d79fadaead903ed869b204030d3e7222f56f496
thetloffline <thetloffline@users.noreply.github.com>
2020-03-30 17:33:21 +0300

    apply formating

--- src/components/allCakes.vue
+++ src/components/allCakes.vue
@@ -1,217 +1,229 @@
 <template>
   <section class="cakes-section-wrapper">
-    <div class='cakes-container'>
-        <h1>Bun stats</h1>
+    <div class="cakes-container">
+      <h1>Bun stats</h1>
       <section class="sort-controls">
-        <!-- <h3>Sort the cakes by</h3> -->
         <ul class="sort-bts">
-          <li class="btn-container" >
-            <button class="btn-card btn-transparent btn-sort btn-selected" id="data-sort-taste" @click="setSortParam('taste', $event), setActiveClass($event.target)">Taste</button>
-            </li>
           <li class="btn-container">
-            <button class="btn-card btn-transparent btn-sort" id="data-sort-looks" @click="setSortParam('looks', $event), setActiveClass($event.target)">Looks</button>
-            </li>
+            <button
+              class="btn-card btn-transparent btn-sort btn-selected"
+              id="data-sort-taste"
+              @click="setSortParam('taste', $event), setActiveClass($event.target)"
+            >Taste</button>
+          </li>
           <li class="btn-container">
-            <button class="btn-card btn-transparent btn-sort" id="data-sort-bun" @click="setSortParam('bun', $event), setActiveClass($event.target)">Bun</button>
-            </li>
+            <button
+              class="btn-card btn-transparent btn-sort"
+              id="data-sort-looks"
+              @click="setSortParam('looks', $event), setActiveClass($event.target)"
+            >Looks</button>
+          </li>
           <li class="btn-container">
-            <button class="btn-card btn-transparent btn-sort" id="data-sort-price" @click="setSortParam('price', $event), setActiveClass($event.target)">€</button>
-            </li>
-        
-        </ul>  
+            <button
+              class="btn-card btn-transparent btn-sort"
+              id="data-sort-bun"
+              @click="setSortParam('bun', $event), setActiveClass($event.target)"
+            >Bun</button>
+          </li>
+          <li class="btn-container">
+            <button
+              class="btn-card btn-transparent btn-sort"
+              id="data-sort-price"
+              @click="setSortParam('price', $event), setActiveClass($event.target)"
+            >€</button>
+          </li>
+        </ul>
       </section>
 
-      <transition-group name='fade'>
-        <div class='cake card'
-          v-for='(cake, index) in sortedCakes'
-          v-bind:class='{lastCake : cake.isLast === true}'
-          v-bind:id='index'
-          v-bind:item='cake'
-          v-bind:index='index'
-          v-bind:key='cake._id'
-          v-bind:ref="cake._id">
+      <transition-group name="fade">
+        <div
+          class="cake card"
+          v-for="(cake, index) in sortedCakes"
+          v-bind:class="{lastCake : cake.isLast === true}"
+          v-bind:id="index"
+          v-bind:item="cake"
+          v-bind:index="index"
+          v-bind:key="cake._id"
+          v-bind:ref="cake._id"
+        >
+          <div
+            class="cake-photo"
+            :style="{ backgroundImage: `url('${getImagePath(cake.imageFile)}')` }"
+          ></div>
 
-          <div class="cake-photo" :style="{ backgroundImage: `url('${getImagePath(cake.imageFile)}')` }"></div>
-          
-          <button class='btn-card btn-card-delete' v-on:click='deleteCake(cake._id)'>Delete</button>
-          
+          <button class="btn-card btn-card-delete" v-on:click="deleteCake(cake._id)">Delete</button>
+
           <ul class="cake-stats">
             <li>
               TASTE
-              <p class=''>{{cake.taste}}</p>
+              <p class>{{cake.taste}}</p>
             </li>
             <li>
               BUN
-              <p class=''>{{cake.bun}}</p>
+              <p class>{{cake.bun}}</p>
             </li>
             <li>
               LOOKS
-              <p class=''>{{cake.looks}}</p>
+              <p class>{{cake.looks}}</p>
             </li>
 
             <!-- <p class='date' v-if='cake.modified'>{{cake.modified}}</p>
-            <p class='date' v-else-if='cake.createdAt'>{{cake.createdAt}}</p> -->
+            <p class='date' v-else-if='cake.createdAt'>{{cake.createdAt}}</p>-->
           </ul>
-          
+
           <div class="container-rating-btns">
             <div class="btn-container">
-              <button class='btn-card btn-dislike' v-on:click.once='rateCake(cake, -1)'>Buns down!</button>
+              <button class="btn-card btn-dislike" v-on:click.once="rateCake(cake, -1)">Buns down!</button>
             </div>
-            <div class='btn-container'>
-              <button class='btn-card btn-like' v-on:click.once='rateCake(cake, 1)'>Buns up!</button>
+            <div class="btn-container">
+              <button class="btn-card btn-like" v-on:click.once="rateCake(cake, 1)">Buns up!</button>
             </div>
           </div>
 
           <div class="cake-description-container">
             <div class="cake-description">
-            <div v-if="cake.price" class="cake-price">
-              {{cake.price}} €
+              <div v-if="cake.price" class="cake-price">{{cake.price}} €</div>
+              <div class="cake-cafe-name">{{cake.cafeName | capitalize}}</div>
+              <div
+                v-if="cake.comment"
+                v-on:click.prevent="toggleSelectedCommentId(index)"
+                class="cake-comment-icon"
+              ></div>
             </div>
-              <div class='cake-cafe-name'>{{cake.cafeName | capitalize}}</div>
-              <div v-if="cake.comment" 
-              v-on:click.prevent="toggleSelectedCommentId(index)"
-              class="cake-comment-icon">
-              </div>
+            <div class="cake-location">
+              <div class="cake-location-icon"></div>
+              <div class="cake-location-address">{{cake.location | capitalize}}</div>
             </div>
-            <div class="cake-location"> 
-              <div class='cake-location-icon'></div>
-              <div class='cake-location-address'>{{cake.location | capitalize}}</div>
-            </div>
-          
           </div>
-          <div v-if="selectedCommentIndex ===index && showComment"
-            class="comment-container" 
-            >
+          <div v-if="selectedCommentIndex ===index && showComment" class="comment-container">
             <div class="cake-comment">
               <p>{{cake.createdAt}}</p>
               <p class="comment">{{cake.comment}}</p>
             </div>
           </div>
-          
         </div>
       </transition-group>
-
     </div>
   </section>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import store from '../store/store'
-import FileUploadService from '../FileUploadService.js'
+import { mapState } from "vuex";
+import store from "../store/store";
+import FileUploadService from "../FileUploadService.js";
 
 export default {
   store,
-  name: 'allCakes',
+  name: "allCakes",
   props: {},
-  data () {
+  data() {
     return {
-      selectedCommentIndex: '',
-      showComment: '',
+      selectedCommentIndex: "",
+      showComment: "",
       sortAsc: {
         type: Boolean,
         value: true
-        },
+      },
       selectedBtn: null,
-      sortParam: 'taste',
+      sortParam: "taste",
       isLast: {
         type: Boolean,
         value: false
       }
-    }
+    };
   },
 
-  mounted () {},
-  created () {
-    const sortButtons = document.querySelectorAll('.btn-sort')
+  mounted() {},
+  created() {
+    const sortButtons = document.querySelectorAll(".btn-sort");
   },
 
   methods: {
-    async deleteCake (id) {
-       await this.$store.dispatch("deleteOne", id)
+    async deleteCake(id) {
+      await this.$store.dispatch("deleteOne", id);
     },
 
-    getImagePath (path) {
+    getImagePath(path) {
       try {
-        return require('./../assets/' + path)
+        return require("./../assets/" + path);
       } catch (error) {
-        console.log('image replaced with placeholder. ', error.message)
-        return require('./../assets/tuuletasku.jpg')
+        console.log("image replaced with placeholder. ", error.message);
+        return require("./../assets/tuuletasku.jpg");
       }
     },
 
-    setActiveClass (e) {
-      const activeBtns = document.querySelectorAll('.btn-selected')
-      activeBtns[0].className = activeBtns[0].className.replace('btn-selected', '')
-      e.classList.add('btn-selected')
+    setActiveClass(e) {
+      const activeBtns = document.querySelectorAll(".btn-selected");
+      activeBtns[0].className = activeBtns[0].className.replace(
+        "btn-selected",
+        ""
+      );
+      e.classList.add("btn-selected");
     },
 
-    setSortParam (sortParam, e) {
-      this.sortParam = sortParam
-    
-      //check if same sort button is pressed
+    setSortParam(sortParam, e) {
+      this.sortParam = sortParam;
+      //check if the same sort button is pressed
       if (this.selectedBtn === e.target.id) {
-        this.sortAsc.value = !this.sortAsc.value
-      } 
-      this.selectedBtn = e.target.id
+        this.sortAsc.value = !this.sortAsc.value;
+      }
+      this.selectedBtn = e.target.id;
     },
 
-    toggleSelectedCommentId (id) {
-      const selector = document.querySelector("#" + CSS.escape(id) )//.childNodes[12].childNodes[0].childNodes[2]
+    toggleSelectedCommentId(id) {
+      const selector = document.querySelector("#" + CSS.escape(id));
       if (this.selectedCommentIndex === id) {
-        this.selectedCommentIndex = ''
-        this.showComment = false
-      } 
-      else {
-        this.showComment = true
-        this.selectedCommentIndex = id
-
-        this.SmoothVerticalScrolling(selector, 275, "center")
+        this.selectedCommentIndex = "";
+        this.showComment = false;
+      } else {
+        this.showComment = true;
+        this.selectedCommentIndex = id;
+        this.scrollToComment(selector);
+        //this.SmoothVerticalScrolling(selector, 275, "center")
       }
     },
 
     scrollToComment(selector) {
-      setTimeout(() => {  
+      setTimeout(() => {
         selector.scrollIntoView({
-          behavior: 'smooth',
-          block: 'end'
-        })
-      }, 90)
+          behavior: "smooth",
+          block: "end"
+        });
+      }, 90);
     },
 
-    rateCake (cake, val) {
-      cake.taste = Number(cake.taste) + Number(val)
-      const formData = new FormData()
-      formData.append('taste', cake.taste)
-      formData.append('id', cake._id)
-      this.$store.dispatch('rateCake', formData)
+    rateCake(cake, val) {
+      cake.taste = Number(cake.taste) + Number(val);
+      const formData = new FormData();
+      formData.append("taste", cake.taste);
+      formData.append("id", cake._id);
+      this.$store.dispatch("rateCake", formData);
     }
   },
-    computed: {
-    ...mapState([
-      'cakes'
-    ]),
+  computed: {
+    ...mapState(["cakes"]),
 
-    sortedCakes: function () {
+    sortedCakes: function() {
       if (this.cakes.length !== 0) {
-        const sortedCakes = this.cakes
+        const sortedCakes = this.cakes;
 
         //identify latest created cake
-        sortedCakes[sortedCakes.length-1].isLast = true
-        
-        //toggle sort DESC ASC 
+        sortedCakes[sortedCakes.length - 1].isLast = true;
+
+        //toggle sort DESC ASC
         let ascDesc = this.sortAsc.value ? -1 : 1;
-        
-        return sortedCakes.sort((a, b) => ascDesc * (Number(a[this.sortParam]) > Number(b[this.sortParam]) ? 1 : -1)) 
-            //return ascDesc * Number(a[this.sortParam].localeCompare(b[this.sortParam]))}
+        return sortedCakes.sort(
+          (a, b) =>
+            ascDesc *
+            (Number(a[this.sortParam]) > Number(b[this.sortParam]) ? 1 : -1)
+        );
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
-
 .content-center {
   display: flex;
   flex-direction: row;
@@ -240,7 +252,7 @@ export default {
 }
 .cover-img {
   height: 67vh;
-  background: url(./../assets/tuuletasku.jpg) no-repeat center center ;
+  background: url(./../assets/tuuletasku.jpg) no-repeat center center;
   background-size: cover;
 }
 .card {
@@ -282,10 +294,10 @@ export default {
   justify-content: space-around;
   flex-grow: 1;
 }
-.cake-stats>div {
-  margin: 0 30px
+.cake-stats > div {
+  margin: 0 30px;
 }
-.cake-stats>li>p {
+.cake-stats > li > p {
   margin: 0 auto 5px;
   font-weight: 700;
 }
@@ -328,7 +340,7 @@ export default {
 .date {
   font-size: 1em;
 }
-.comment-container{
+.comment-container {
   display: flex;
   margin: 0 24px;
 }
@@ -346,15 +358,16 @@ export default {
   cursor: pointer;
 }
 /* SORT */
-.sort-controls>ul, .sort-controls>ul>li  {
+.sort-controls > ul,
+.sort-controls > ul > li {
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   margin-top: 5vh;
 }
 .sort-bts {
-justify-content: space-around;
-margin: auto;
+  justify-content: space-around;
+  margin: auto;
 }
 
 /* CONTAINER */
@@ -380,10 +393,10 @@ margin: auto;
 }
 .btn-like {
   color: white;
-  background-image: linear-gradient(to right, #39dc7a , #12d67e);
+  background-image: linear-gradient(to right, #39dc7a, #12d67e);
 }
 .btn-like:active {
-  background-image: linear-gradient(to right, #36e97e , #12d67e);
+  background-image: linear-gradient(to right, #36e97e, #12d67e);
 }
 .btn-dislike {
   color: white;
@@ -405,11 +418,11 @@ margin: auto;
 }
 
 /* FADE TRANSITION */
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
-
 </style>

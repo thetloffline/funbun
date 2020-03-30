4bc7327e3c3fe08df247ce9892ea62421a05fe28
thetloffline <thetloffline@users.noreply.github.com>
2020-03-30 17:47:15 +0300

    apply formatting

--- src/components/RangeSliderComponent.vue
+++ src/components/RangeSliderComponent.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class='card range'>
-    <div class='label-for'>
+  <div class="card range">
+    <div class="label-for">
       <label>{{ label }}</label>
     </div>
     <input
-      type='range'
-      :id='id'
-      :value='value'
-      :placeholder='label'
-      min='0'
-      max='100'
-      step='1'
-      v-on:mousemove='getValue($event)'
-      v-on:input='getValue($event)'
-      v-on:mousedown='mousePressed()'
-      v-on:mouseup='mouseUp()'
+      type="range"
+      :id="id"
+      :value="value"
+      :placeholder="label"
+      min="0"
+      max="100"
+      step="1"
+      v-on:mousemove="getValue($event)"
+      v-on:input="getValue($event)"
+      v-on:mousedown="mousePressed()"
+      v-on:mouseup="mouseUp()"
     />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'rangeSlider',
+  name: "rangeSlider",
   props: {
     id: {
       type: String
@@ -43,56 +43,56 @@ export default {
       rangeValue: {
         type: Number
       },
-      error: ''
-    }
+      error: ""
+    };
   },
 
   methods: {
     mousePressed() {
-      this.clicked = true
+      this.clicked = true;
     },
     mouseUp() {
-      this.clicked = false
+      this.clicked = false;
     },
     async getValue(event) {
       if ((await this.clicked) === true) {
-        let value = parseInt(event.target.value)
-        this.$emit('input', value)
-        this.rangeValue = value
-      } 
+        let value = Number(event.target.value);
+        this.$emit("input", value);
+        this.rangeValue = value;
+      }
     }
   }
-}
+};
 </script>
 
 <style scoped>
 label {
   display: flex;
 }
-input[type='range'] {
+input[type="range"] {
   width: 100%;
-  padding: 10px 0px;
+  padding: 10px 0;
   outline: none;
 }
-input[type='range'] {
+input[type="range"] {
   -webkit-appearance: none;
   margin: 18px 0;
   width: 100%;
   background-color: transparent;
 }
-input[type='range']:focus {
+input[type="range"]:focus {
   outline: none;
 }
-input[type='range']::-webkit-slider-runnable-track {
+input[type="range"]::-webkit-slider-runnable-track {
   width: 100%;
   height: 2px;
   cursor: pointer;
   animate: 0.2s;
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
   background: midnightblue;
 }
-input[type='range']::-webkit-slider-thumb {
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+input[type="range"]::-webkit-slider-thumb {
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
   border: 1px solid #ffffff;
   height: 18px;
   width: 18px;
@@ -102,20 +102,20 @@ input[type='range']::-webkit-slider-thumb {
   -webkit-appearance: none;
   margin-top: -8px;
 }
-input[type='range']:focus::-webkit-slider-runnable-track {
+input[type="range"]:focus::-webkit-slider-runnable-track {
   background: midnightblue;
   border-color: midnightblue;
 }
-input[type='range']::-moz-range-track {
+input[type="range"]::-moz-range-track {
   width: 100%;
   height: 2px;
   cursor: pointer;
   animate: 0.2s;
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
   background: midnightblue;
 }
-input[type='range']::-moz-range-thumb {
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+input[type="range"]::-moz-range-thumb {
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
   border: 1px solid #ffffff;
   height: 18px;
   width: 18px;
@@ -123,7 +123,7 @@ input[type='range']::-moz-range-thumb {
   background: midnightblue;
   cursor: pointer;
 }
-input[type='range']::-ms-track {
+input[type="range"]::-ms-track {
   width: 100%;
   height: 8.4px;
   cursor: pointer;
@@ -133,20 +133,20 @@ input[type='range']::-ms-track {
   border-width: 16px 0;
   color: transparent;
 }
-input[type='range']::-ms-fill-lower {
+input[type="range"]::-ms-fill-lower {
   background: #2a6495;
   border: 0.2px solid #010101;
   border-radius: 2.6px;
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
 }
-input[type='range']::-ms-fill-upper {
+input[type="range"]::-ms-fill-upper {
   background: darkgrey;
   border: 0.2px solid #010101;
   border-radius: 2.6px;
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
 }
-input[type='range']::-ms-thumb {
-  /*   box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; */
+input[type="range"]::-ms-thumb {
+  /*   box-shadow: 1px 1px 1px #000000, 0 0 1px #0d0d0d; */
   border: 1px solid #ffffff;
   height: 18px;
   width: 18px;
@@ -154,10 +154,10 @@ input[type='range']::-ms-thumb {
   background: midnightblue;
   cursor: pointer;
 }
-input[type='range']:focus::-ms-fill-lower {
+input[type="range"]:focus::-ms-fill-lower {
   background: midnightblue;
 }
-input[type='range']:focus::-ms-fill-upper {
+input[type="range"]:focus::-ms-fill-upper {
   background: midnightblue;
 }
 </style>

5ebd07ae070022de83ad964cb691b0aa3b254d37
thetloffline <thetloffline@users.noreply.github.com>
2020-03-30 21:16:01 +0300

    make cake.location suitable for google.maps query

--- src/main.js
+++ src/main.js
@@ -15,6 +15,12 @@ Vue.filter('capitalize', function (value) {
   return value.charAt(0).toUpperCase() + value.slice(1)
 })
 
+Vue.filter('address', function (value) {
+  if (!value) return ''
+  value = value.toString()
+  return value.replace(' ', '+')
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',

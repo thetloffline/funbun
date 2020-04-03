c3833d35ed0f086e9cc52cb2e356747f34f331e9
thetloffline <thetloffline@users.noreply.github.com>
2020-04-03 16:28:13 +0300

    catch and log errors to console

--- be/routes/api/files.js
+++ be/routes/api/files.js
@@ -1,33 +1,36 @@
 const express = require('express')
-const fileUpload = require('express-fileupload')
 const mongodb = require('mongodb')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const cakes = await loadCakesCollection()
-  await cakes.find( {} ).toArray((err, result) => {
-    if (err) throw err
-    res.send(result)
-  });
-})  
+  try {
+    const cakes = await loadCakesCollection()
+    await cakes.find({}).toArray((err, result) => {
+      if (err) throw err
+      res.send(result)
+    })
+  } catch (err) {
+    console.log(' failed to load cakes collection. error ::::: ', err)
+  }
+})
 
 router.post('/', async (req, res) => {
   const db = await loadCakesCollection()
   const file = req.files['files[0]']
   const filePath = __dirname + '/../../../src/assets/' + file.name
-  const fileName = file.name;
+  const fileName = file.name
   const cake = {
-    "cafeName" : req.body.cafeName,
-    "location" : req.body.location,
-    "price" : req.body.price,
-    "comment" : req.body.comment,
-    "imageFile" : fileName, 
-    "looks" : req.body.looks,
-    "taste" : req.body.taste,
-    "bun" : req.body.bun,
-    "createdAt" : new Date()
+    cafeName: req.body.cafeName,
+    location: req.body.location,
+    price: req.body.price,
+    comment: req.body.comment,
+    imageFile: fileName,
+    looks: req.body.looks,
+    taste: req.body.taste,
+    bun: req.body.bun,
+    createdAt: new Date()
   }
-
+  // validate joi
   await file.mv(filePath, (err) => {
     if (err) {
       return res.sendStatus(400).send(err)
@@ -48,31 +51,38 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     const cakes = await loadCakesCollection()
-    const result = await cakes.updateOne( {_id: new mongodb.ObjectID(req.params.id)}, { $set : {'taste' : req.body.taste+''}} )
+    const result = await cakes.updateOne({ _id: new mongodb.ObjectID(req.params.id) }, { $set: { taste: req.body.taste + '' } })
     res.sendStatus(200)
-    //await  cakes.updateOne( {_id: req.params.id}, { $set : { "modified" : new Date()} } )
-  } catch (error) {
-    console.log(error)
+    // await  cakes.updateOne( {_id: req.params.id}, { $set : { "modified" : new Date()} } )
+  } catch (err) {
+    console.log('failed to update cake with id: ', req.params.id)
+    console.log('error :::::: ', err)
   }
 })
 
 router.delete('/:id', async (req, res) => {
   try {
     const cakes = await loadCakesCollection()
-    await  cakes.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    await cakes.deleteOne({ _id: new mongodb.ObjectID(req.params.id) })
     res.sendStatus(200)
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log('failed to delete cake with id: ', req.params.id)
+    console.log('error :::::: ', err)
   }
 })
 
-async function loadCakesCollection() {
-  const client = await require('mongodb').MongoClient.connect('mongodb://localhost:27017/express', 
-  { 
-    useNewUrlParser: true,  
-    useUnifiedTopology: true 
-  })
-  return db = client.db('express').collection('cakes')
+async function loadCakesCollection () {
+  try {
+    const client = await require('mongodb').MongoClient.connect('mongodb://localhost:27017/express',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    const db = client.db('express').collection('cakes')
+    return db
+  } catch (err) {
+    console.log('failed to connect to Mongodb :::::: ', err)
+  }
 }
 
 module.exports = router
